aa0a07b0d3d6cd3bf20b48cf5296cad64a6e8d6b
canyoufeelme <43756723+canyoufeelme@users.noreply.github.com>
2018-12-10 12:35:10 -0500

    Add files via upload

---
+++ Problem2.py
@@ -0,0 +1,6 @@
+name=input('what is your name? ')
+print('Nice to meet you ' + name + ", " + " my name is Chatbot.")
+age=input('How old are you?')
+print('Wow,' " " + age + " " "years old! I was born today.")
+mood=input('How are you feeling today?')
+print('Glad to hear you are feeling' " " + mood + "." "That is Wonderful to hear!")
